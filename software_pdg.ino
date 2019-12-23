9922e7cab93694450f5482d81028e6db15b6429c
Steve Carter <sweavo@gmail.com>
2019-12-23 20:13:59 +0000

    organize file layout

--- software_pdg.ino
+++ software_pdg.ino
@@ -1,28 +1,58 @@
 #include <Adafruit_NeoPixel.h>
 
+//////////////////////////////////////////////////////////////////////////////////
+// Commissioning parameters
+
+// Attach something with WS2812 to this pin
 #define STRIP_PIN 4
 #define LED_COUNT 30
 
+// Pushbutton to play the game.  If active high, then 
+// set BUTTON_ACTIVE_LOW to 0 and deal with pulldown as
+// you need to.  If active low, then setup() will activate
+// the input's builtin pullup resistor.
 #define BUTTON_PIN 10
 #define BUTTON_ACTIVE_LOW 1
 
+// Attach a piezo speaker between this pin and GND.
 #define PIEZO_PIN 9
 
+// This is a diagnostic pin
 #define LED_PIN   LED_BUILTIN
 
-Adafruit_NeoPixel strip(LED_COUNT, STRIP_PIN, NEO_GRB + NEO_KHZ800);
+// End of commissioning parameters
+//////////////////////////////////////////////////////////////////////////////////
+// Tuning parameters
 
+// "Stylesheet"
+//// Globally dim values, so that you can work on the program without getting a migraine.
+#define BRIGHTNESS_DIVIDER 4
+//// Theme Colors
+#define COLOR_ETAS_BLUE strip.Color(20/BRIGHTNESS_DIVIDER, 20/BRIGHTNESS_DIVIDER, 80/BRIGHTNESS_DIVIDER)
+#define COLOR_BLACK strip.Color(0,0,0)
+#define COLOR_WHITE strip.Color(40/BRIGHTNESS_DIVIDER,40/BRIGHTNESS_DIVIDER,40/BRIGHTNESS_DIVIDER)
+#define COLOR_RED strip.Color(40/BRIGHTNESS_DIVIDER, 0, 0)
+//// Semantic colors
+#define COLOR_WORKPACKAGE_OK COLOR_ETAS_BLUE
+#define COLOR_WORKPACKAGE_FAIL COLOR_RED
+
+// Tuning values for the game 
+#define ERROR_PERCENT 10
+#define STRIDE 6
+
+// End of tuning parameters
+//////////////////////////////////////////////////////////////////////////////////
+
+// Top-level states
 #define GAME_WAIT 0
 #define GAME_RUN 1
 #define GAME_SCORE 2
 #define GAME_RESET 3
 int game_state = GAME_WAIT;
 
-// the game
-#define ERROR_PERCENT 10
+// State used while playing the game
 bool successful[LED_COUNT];
 int committed = 0;
-#define STRIDE 6
 int player_pos = 0;
 int player_dir = 1;
 
@@ -35,12 +65,15 @@ bool random_bit() {
   return random(100) > ERROR_PERCENT;
 }
 
-#define BRIGHTNESS_DIVIDER 4
-#define COLOR_ETAS_BLUE strip.Color(20/BRIGHTNESS_DIVIDER, 20/BRIGHTNESS_DIVIDER, 80/BRIGHTNESS_DIVIDER)
-#define COLOR_BLACK strip.Color(0,0,0)
-#define COLOR_WHITE strip.Color(40/BRIGHTNESS_DIVIDER,40/BRIGHTNESS_DIVIDER,40/BRIGHTNESS_DIVIDER)
-#define COLOR_WORKPACKAGE_OK COLOR_ETAS_BLUE
-#define COLOR_WORKPACKAGE_FAIL strip.Color(40/BRIGHTNESS_DIVIDER, 0, 0)
+void beep(unsigned char period)
+{
+  analogWrite(PIEZO_PIN,20);
+  delay( period/2);
+  analogWrite(PIEZO_PIN,0);
+  delay( period/2);
+}
+
+Adafruit_NeoPixel strip(LED_COUNT, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 
 void light_led( int posn ) {
   // light the LED in position posn according to its successful status.
@@ -54,6 +87,19 @@ void light_led( int posn ) {
 
 void extinguish_led( int posn ) {
   strip.setPixelColor( posn, COLOR_BLACK );
+}
+
+void setup() {
+  randomSeed(analogRead(5)); // randomize using noise from analog pin 5
+  strip.begin();
+  strip.show();
+  pinMode(LED_PIN, OUTPUT);
+  #if BUTTON_ACTIVE_LOW==1
+  pinMode(BUTTON_PIN,INPUT_PULLUP);
+  #else
+  pinMode(BUTTON_PIN,INPUT);
+  #endif
+  pinMode(PIEZO_PIN, OUTPUT);
 }
 
 bool game_raster() {
@@ -85,6 +131,7 @@ bool game_raster() {
   strip.show();
   return false;
 }
+
 bool score_animation_done() {
   static uint8_t i=0;
   strip.setPixelColor( i, COLOR_WHITE );
@@ -102,24 +149,6 @@ bool score_animation_done() {
   }
   return false;
 }
-
-void setup() {
-  randomSeed(analogRead(5)); // randomize using noise from analog pin 5
-  strip.begin();
-  strip.show();
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN,INPUT_PULLUP);
-  pinMode(PIEZO_PIN, OUTPUT);
-}
-
-void beep(unsigned char period)
-{
-  analogWrite(PIEZO_PIN,20);
-  delay( period/2);
-  analogWrite(PIEZO_PIN,0);
-  delay( period/2);
-}
-
 
 void loop() {
   // key_pressed can be reset by the reader. key_down is used to detect edges
